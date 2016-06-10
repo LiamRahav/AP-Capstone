e27dc6960fffa033a3c664630984ab9f53234529
Shawn Robin <smrobin623@gmail.com>
2016-06-10 12:58:42 -0400

    trying to find the error

--- src/birthdays/BirthdaysSpeechletRequestStreamHandler.java
+++ src/birthdays/BirthdaysSpeechletRequestStreamHandler.java
@@ -5,16 +5,15 @@ import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
 import java.util.HashSet;
 import java.util.Set;
 
-public class BirthdaysSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler {
+public final class BirthdaysSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler {
 
-    private static final Set<String> supportedApplicationIds;
+    private static final Set<String> supportedApplicationIds = new HashSet<String>();
 
     static {
         /*
          * This Id can be found on https://developer.amazon.com/edw/home.html#/ "Edit" the relevant
          * Alexa Skill and put the relevant Application Ids in this Set.
          */
-        supportedApplicationIds = new HashSet<String>();
         supportedApplicationIds.add("amzn1.echo-sdk-ams.app.f5ad9710-4e6f-4a2e-8a5f-aceb7033fcf2");
     }
 
